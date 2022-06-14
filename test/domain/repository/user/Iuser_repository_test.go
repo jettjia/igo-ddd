2945855f1e262dc38ff6f88e4ef2e4ca6f95b509
“jettjia” <jettjia@foxmail.com>
2022-06-14 10:23:32 +0800

    fate:gomock

--- test/domain/repository/user/Iuser_repository_test.go
+++ test/domain/repository/user/Iuser_repository_test.go
@@ -4,35 +4,37 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/magiconair/properties/assert"
 	. "github.com/smartystreets/goconvey/convey"
 
 	_ "github.com/jett/gin-ddd/boot"
 	entity "github.com/jett/gin-ddd/domain/entity/user"
 	"github.com/jett/gin-ddd/domain/repository/user/mock"
+	dao "github.com/jett/gin-ddd/infrastructure/dao/user"
 )
 
 // go test -cover ./...
-func Test_User(t *testing.T) {
+func TestUser_User(t *testing.T) {
 	ctrl := gomock.NewController(t) // 初始化 controller
 	defer ctrl.Finish()
 
 	userRepo := mock.NewMockIUserRepository(ctrl) // 初始化 mock
 
-	//userDao := dao.UserDao{}
+	userDao := dao.UserDao{}
 
-	Convey("Convey Test Get Userinfo repository", t, func() {
+	Convey("Convey Test Get Userinfo dao", t, func() {
 		var err error
 		var id uint64
 
-		Convey("Get Userinfo Log Success", func() {
+		Convey("Get Userinfo Success", func() {
 			err = nil
 			id = 100
 			repoDataRes := &entity.User{}
 			repoDataRes.ID = id
-			userRepo.EXPECT().GetUser(id).Return(repoDataRes, err)
-			//dataRes, errRes := userDao.GetUser(id)
-			//assert.Equal(t, dataRes.ID, id)
-			//assert.Equal(t, errRes, err)
+			userRepo.EXPECT().GetUser(id).AnyTimes().Return(repoDataRes, err)
+			dataRes, errRes := userDao.GetUser(id)
+			assert.Equal(t, dataRes.ID, id)
+			assert.Equal(t, errRes, err)
 		})
 	})
 }
