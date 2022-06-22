a81e2fd51c094ceb7d5acb6dbfbad49e615833d9
“jettjia” <jettjia@foxmail.com>
2022-06-22 17:40:28 +0800

    fix:移除无用依赖

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	_ "github.com/jett/gin-ddd/boot"
 	"github.com/jett/gin-ddd/cmd"
 	"github.com/jett/gin-ddd/interfaces/grpc"
-	_ "github.com/jett/gin-ddd/interfaces/grpc"
 	"github.com/jett/gin-ddd/interfaces/http"
-	_ "github.com/jett/gin-ddd/interfaces/http"
 )
 
 func main() {
