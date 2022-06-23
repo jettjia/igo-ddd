0ab7f85889bd247914bc396a91e524c9df7c8f9f
“jettjia” <jettjia@foxmail.com>
2022-06-23 16:22:12 +0800

    test:db

--- test/infrastructure/pkg/mysql/db_test.go
+++ test/infrastructure/pkg/mysql/db_test.go
@@ -1,21 +1,15 @@
 package mysql
 
 import (
-	"fmt"
 	"testing"
 
 	_ "github.com/jett/gin-ddd/boot"
 	"github.com/jett/gin-ddd/global"
 )
 
-type User struct {
-	Id       int64  `gorm:"primarykey;type:int;comment:主键" json:"id"`
-	Nickname string `gorm:"type:varchar(32); not null; comment昵称;" json:"nickname"`
-}
-
 func TestInitDB(t *testing.T) {
-	user := User{}
-	global.GDB.First(user, 1)
-
-	fmt.Printf("%+v", user)
+	err := global.GDB.DB().Ping()
+	if err != nil {
+		t.Fatal("mysql conn err :", err)
+	}
 }
