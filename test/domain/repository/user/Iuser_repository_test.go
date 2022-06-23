29e47e05cac114f139c7c54fb5bc2478122d05b7
“jettjia” <jettjia@foxmail.com>
2022-06-23 10:06:48 +0800

    fate:test

--- test/domain/repository/user/Iuser_repository_test.go
+++ test/domain/repository/user/Iuser_repository_test.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"context"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -15,6 +16,7 @@ import (
 
 // go test -cover ./...
 func TestUser_User(t *testing.T) {
+	ctx := context.Background()
 	ctrl := gomock.NewController(t) // 初始化 controller
 	defer ctrl.Finish()
 
@@ -31,8 +33,8 @@ func TestUser_User(t *testing.T) {
 			id = 100
 			repoDataRes := &entity.User{}
 			repoDataRes.ID = id
-			userRepo.EXPECT().GetUser(id).AnyTimes().Return(repoDataRes, err)
-			dataRes, errRes := userDao.GetUser(id)
+			userRepo.EXPECT().GetUser(ctx, id).AnyTimes().Return(repoDataRes, err)
+			dataRes, errRes := userDao.GetUser(ctx, id)
 			assert.Equal(t, dataRes.ID, id)
 			assert.Equal(t, errRes, err)
 		})
