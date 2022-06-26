5a85e2ebec091a3a7e52ecaa5f326b29935c206e
“jettjia” <jettjia@foxmail.com>
2022-06-26 11:58:21 +0800

    test(nsq):test nsq ping

--- test/infrastructure/pkg/mq/nsq/nsq_test.go
+++ test/infrastructure/pkg/mq/nsq/nsq_test.go
@@ -5,15 +5,20 @@ import (
 	"testing"
 
 	_ "github.com/jett/gin-ddd/boot"
-	"github.com/jett/gin-ddd/global"
+	"github.com/jett/gin-ddd/infrastructure/pkg/mq/nsq"
 )
 
 func TestNsq_NewNsqProducer(t *testing.T) {
-	err := global.GNsqProducer.Ping()
+	client := nsq.NewNsqClient()
+	cp, err := client.NsqProducer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = cp.Ping()
 	if nil != err {
 		// 关闭生产者
-		global.GNsqProducer.Stop()
-		global.GNsqProducer = nil
+		cp.Stop()
+		cp = nil
 		t.Fatal(err)
 	}
 
