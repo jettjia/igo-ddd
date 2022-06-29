890d201b8255d2d270c5aff9e8971d1707b0273e
“jettjia” <jettjia@foxmail.com>
2022-06-29 15:32:53 +0800

    fate:Modify the domain/agg as an interface

--- domain/aggregate/user/user_agg.go
+++ domain/aggregate/user/user_agg.go
@@ -7,35 +7,50 @@ import (
 	repository "github.com/jett/gin-ddd/domain/irepository/user"
 )
 
+type UserAgg interface {
+	AddUser(context.Context) error // 添加用户
+}
+
+var _ UserAgg = (*userAgg)(nil)
+
 // UserAgg 会有聚合：用户+用户日志
-type UserAgg struct {
-	User        *entity.User
-	UserLog     *entity.UserLog
+type userAgg struct {
+	user        *entity.User
+	userLog     *entity.UserLog
 	userRepo    repository.IUserRepository
 	userRepoLog repository.IUserLogRepository
 }
 
-// Create 创建会员,创建的时候会增加日志
-func (u *UserAgg) Create(ctx context.Context) error {
-	entityUser, err := u.userRepo.SaveUser(ctx, u.User)
+// AddUser 创建会员,创建的时候会增加日志
+func (u *userAgg) AddUser(ctx context.Context) error {
+	entityUser, err := u.userRepo.SaveUser(ctx, u.user)
 	if err != nil {
 		return err
 
 	}
 
-	u.UserLog.UserId = entityUser.ID
-	u.UserLog.Log = "创建用户" + entityUser.Nickname
-	_, err = u.userRepoLog.SaveLog(ctx, u.UserLog)
+	u.userLog.UserId = entityUser.ID
+	u.userLog.Log = "创建用户" + entityUser.Nickname
+	_, err = u.userRepoLog.SaveLog(ctx, u.userLog)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (u *UserAgg) GetLogs() (ret []*entity.UserLog) {
-	return
-}
+// Create 创建会员,创建的时候会增加日志
+func (u *userAgg) Create(ctx context.Context) error {
+	entityUser, err := u.userRepo.SaveUser(ctx, u.user)
+	if err != nil {
+		return err
 
-func (u *UserAgg) GetUserInfo() {
+	}
 
+	u.userLog.UserId = entityUser.ID
+	u.userLog.Log = "创建用户:" + entityUser.Nickname
+	_, err = u.userRepoLog.SaveLog(ctx, u.userLog)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- domain/service/user/user_service.go
+++ domain/service/user/user_service.go
@@ -3,9 +3,8 @@ package user
 import (
 	"context"
 	entity "github.com/jett/gin-ddd/domain/entity/user"
-	dao "github.com/jett/gin-ddd/infrastructure/repository/repositoryimpl/user"
-
 	repository "github.com/jett/gin-ddd/domain/irepository/user"
+	dao "github.com/jett/gin-ddd/infrastructure/repository/repositoryimpl/user"
 )
 
 // UserService user service interface
@@ -19,12 +18,12 @@ type userService struct {
 	userRepo repository.IUserRepository
 }
 
-func (u *userService) FindUserByID(ctx context.Context, id uint64) (*entity.User, error) {
-	return u.userRepo.GetUser(ctx, id)
-}
-
 func NewUserService() *userService {
 	return &userService{
 		userRepo: dao.NewUserRepo(),
 	}
+}
+
+func (u *userService) FindUserByID(ctx context.Context, id uint64) (*entity.User, error) {
+	return u.userRepo.GetUser(ctx, id)
 }
