a2162614e65b20bb7e9b42532aa20014491ea509
“jettjia” <jettjia@foxmail.com>
2023-04-08 12:46:06 +0800

    feat:日志优化

--- global/global.go
+++ global/global.go
@@ -1,16 +1,16 @@
 package global
 
 import (
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 
 	entity "github.com/jettjia/go-ddd-demo/domain/entity/jwt"
 	"github.com/jettjia/go-ddd-demo/infrastructure/config"
-	"github.com/jettjia/go-ddd-demo/infrastructure/pkg/log"
 )
 
 var (
 	Gconfig       *config.Config      // 全局配置
-	GLog          log.Logger          // 全局log
+	GLog          *logrus.Logger      // 全局log
 	GDB           *gorm.DB            // 全局 DB
 	GCustomerInfo entity.CustomerInfo // 登录的全局信息
 )
--- infrastructure/pkg/log/log.go
+++ infrastructure/pkg/log/log.go
@@ -7,159 +7,23 @@ import (
 	"github.com/jettjia/go-ddd-demo/infrastructure/config"
 )
 
-// Logger 服务日志服务，可适配其他日志组件
-type Logger interface {
-	Infof(format string, args ...interface{})
-	Infoln(args ...interface{})
-	Debugf(format string, args ...interface{})
-	Debugln(args ...interface{})
-	Errorf(format string, args ...interface{})
-	Errorln(args ...interface{})
-	Warnf(format string, args ...interface{})
-	Warnln(args ...interface{})
-	Tracef(format string, args ...interface{})
-	Traceln(args ...interface{})
-	Panicf(format string, args ...interface{})
-	Panicln(args ...interface{})
-	Fatalf(format string, args ...interface{})
-	Fatalln(args ...interface{})
-}
-
-var (
-	l Logger
-)
-
 // NewLogger 获取日志句柄
-func NewLogger(env string) Logger {
-	initLogger(env)
-
-	return l
+func NewLogger(env string) *logrus.Logger {
+	return initLogger(env)
 }
 
-type serverLog struct {
-	logger *logrus.Logger
-}
-
-// Infof 普通信息
-func (l *serverLog) Infof(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Infof(format, args...)
-}
-
-// Infoln 普通信息
-func (l *serverLog) Infoln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Infoln(args...)
-}
-
-// Warnf 警告信息
-func (l *serverLog) Warnf(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Warnf(format, args...)
-}
-
-// Warnln 警告信息
-func (l *serverLog) Warnln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Warnln(args...)
-}
-
-// Errorf 错误信息
-func (l *serverLog) Errorf(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Errorf(format, args...)
-}
-
-// Errorln 错误信息
-func (l *serverLog) Errorln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Errorln(args...)
-}
-
-// Debugf 调试信息
-func (l *serverLog) Debugf(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Debugf(format, args...)
-}
-
-// Debugln 调试信息
-func (l *serverLog) Debugln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Debugln(args...)
-}
-
-// Tracef 跟踪信息
-func (l *serverLog) Tracef(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Tracef(format, args...)
-}
-
-// Traceln 跟踪信息
-func (l *serverLog) Traceln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Traceln(args...)
-}
-
-// Fatalf 致命错误
-func (l *serverLog) Fatalf(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Fatalf(format, args...)
-}
-
-// Fatalln 致命错误
-func (l *serverLog) Fatalln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Fatalln(args...)
-}
-
-// Panicf 恐慌错误
-func (l *serverLog) Panicf(format string, args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Panicf(format, args...)
-}
-
-// Panicln 恐慌错误
-func (l *serverLog) Panicln(args ...interface{}) {
-	if l.logger == nil {
-		return
-	}
-	l.logger.Panicln(args...)
-}
-
-func initLogger(env string) {
+func initLogger(env string) *logrus.Logger {
 	conf := config.NewConfig(env) // 读取配置
 
-	logHandle := &serverLog{}
-	logHandle.logger = logrus.New()
-	logHandle.logger.SetLevel(logLevel(conf.Log.LogLevel))
-	logHandle.logger.WithField("server", conf.Server.ServerName)
-	logHandle.logger.SetFormatter(&logrus.JSONFormatter{})
+	logHandle := logrus.New()
+	logHandle.SetLevel(logLevel(conf.Log.LogLevel))
+	logHandle.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	//logHandle.WithFields(logrus.Fields{
+	//	"server": conf.Server.ServerName,
+	//	"id":     util.Ulid(),
+	//}) // todo
 
 	logDir := conf.Log.LogFileDir
 	if logDir == "" {
@@ -180,9 +44,9 @@ func initLogger(env string) {
 		Compress:   true,                // 是否压缩日志，默认是不压缩。这里设置为true，压缩日志
 	}
 
-	logHandle.logger.SetOutput(loggerOut)
+	logHandle.SetOutput(loggerOut)
 
-	l = logHandle
+	return logHandle
 }
 
 func logLevel(logLevel string) (level logrus.Level) {
